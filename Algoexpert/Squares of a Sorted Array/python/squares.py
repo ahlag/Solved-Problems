90312a3c2abc312c1890b2e1d995061f9681bc64
Chang Yong Lik <theeahlag@gmail.com>
2021-07-27 22:18:00 +0900

    Added sorted two pointers

--- Algoexpert/Squares of a Sorted Array/python/squares.py
+++ Algoexpert/Squares of a Sorted Array/python/squares.py
@@ -9,12 +9,30 @@ def sortedSquaredArray(array):
 
     return result
 
+def sortedSquaredArray_two_pointers(nums):
+
+    n = len(nums)
+    result = [0] * n
+    left = 0
+    right = n - 1
+    for i in range(n - 1, -1, -1):
+        if abs(nums[left]) < abs(nums[right]):
+            square = nums[right]
+            right -= 1
+        else:
+            square = nums[left]
+            left += 1
+        result[i] = square * square
+    return result
+
 
 def main():
     
     array = [75, 105, 120, 75, 90, 135]
     
     print(sortedSquaredArray(array))
+    
+    print(sortedSquaredArray_two_pointers(array))
     
 if __name__ == "__main__":
     main()
