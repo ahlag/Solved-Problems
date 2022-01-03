f3b5eb6bbdf04db365e72553376f3b2a4239d7c1
Chang Yong Lik <theeahlag@gmail.com>
2022-01-03 17:26:23 +0900

    Added Permutations

---
+++ Algoexpert/Permutations/python/permutations.py
@@ -0,0 +1,34 @@
+import unittest
+
+# Upper Bound: O(n^2*n!) time | O(n*n!)
+# Roughly: O(n*n!) time | O(n*n!) space
+def getPermutations(array):
+    permutations = []
+    permutationsHelper(array, [], permutations)
+    return permutations
+
+def permutationsHelper(array, currentPermutation, permutations):
+    if not len(array) and len(currentPermutation):
+        permutations.append(currentPermutation)
+    else:
+        for i in range(len(array)):
+            newArray = array[:i] + array[i + 1:]
+            newPermutation = currentPermutation + [array[i]]
+            permutationsHelper(newArray, newPermutation, permutations)
+
+
+
+class TestProgram(unittest.TestCase):
+    def test_case_1(self):
+        perms = getPermutations([1, 2, 3])
+        self.assertTrue(len(perms) == 6)
+        self.assertTrue([1, 2, 3] in perms)
+        self.assertTrue([1, 3, 2] in perms)
+        self.assertTrue([2, 1, 3] in perms)
+        self.assertTrue([2, 3, 1] in perms)
+        self.assertTrue([3, 1, 2] in perms)
+        self.assertTrue([3, 2, 1] in perms)
+
+if __name__ == "__main__":
+    unittest.main()
+
