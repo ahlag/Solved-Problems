98effe73a04f72a0156376c70e11962e3a2ea3b5
Chang Yong Lik <theeahlag@gmail.com>
2022-02-27 15:34:26 +0900

    Added sum of two integers

---
+++ LeetCode/Two Sum/cpp/two_sum.cpp
@@ -0,0 +1,37 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+class Solution {
+    public:
+        vector<int> twoSum(vector<int>& nums, int& target) {
+            unordered_map<int, size_t>seen;
+
+            for(size_t i = 0; i < nums.size(); i++) {
+                
+                if( seen.count((target - nums[i])) ) {
+                    return {seen[target - nums[i]], i};
+                } else {
+                    seen[nums[i]] = i;
+                }
+            } 
+            
+        }
+
+};
+
+
+int main() {
+
+    vector<int> nums = {2,7,11,15};
+    int target = 9;
+
+    Solution solution;
+    vector<int> res = solution.twoSum(nums, target);
+
+    for(const auto ele: res) {
+        cout << ele << " ";
+    }
+
+    return 0;
+}
