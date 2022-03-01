2a4005dab2073e6cd768d382f5eae62885a916fd
Chang Yong Lik <theeahlag@gmail.com>
2022-03-01 20:02:52 +0900

    Added remove kth node from end java

---
+++ Algoexpert/Remove Nth Node From End/java/Test/RemoveKthNodeFromEndTest.java
@@ -0,0 +1,58 @@
+import org.junit.jupiter.api.Test;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class RemoveKthNodeFromEndTest {
+    @Test
+    public void TestCase1() {
+        TestLinkedList test = new TestLinkedList(0);
+        test.addMany(new int[] {1, 2, 3, 4, 5, 6, 7, 8, 9});
+        int[] expected = {0, 1, 2, 3, 4, 5, 7, 8, 9};
+        RemoveKthNodeFromEnd.removeKthNodeFromEnd(test, 4);
+        assertTrue(compare(test.getNodesInArray(), expected));
+    }
+
+    public boolean compare(List<Integer> arr1, int[] arr2) {
+        if (arr1.size() != arr2.length) {
+            return false;
+        }
+        for (int i = 0; i < arr1.size(); i++) {
+            if (arr1.get(i) != arr2[i]) {
+                return false;
+            }
+        }
+        return true;
+    }
+
+    class TestLinkedList extends LinkedList {
+
+        public TestLinkedList(int value) {
+            super(value);
+        }
+
+        public void addMany(int[] values) {
+            LinkedList current = this;
+            while (current.next != null) {
+                current = current.next;
+            }
+            for (int value : values) {
+                current.next = new LinkedList(value);
+                current = current.next;
+            }
+        }
+
+        public List<Integer> getNodesInArray() {
+            List<Integer> nodes = new ArrayList<Integer>();
+            LinkedList current = this;
+            while (current != null) {
+                nodes.add(current.value);
+                current = current.next;
+            }
+            return nodes;
+        }
+    }
+
+}
