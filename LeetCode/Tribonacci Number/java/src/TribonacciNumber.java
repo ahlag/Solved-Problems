8d3feff5b0b6b50c5b89425a0f7b50036f00657c
Chang Yong Lik <theeahlag@gmail.com>
2022-03-06 23:07:45 +0900

    added tribonacci java

--- LeetCode/Tribonacci Number/java/src/TribonacciNumber.java
+++ LeetCode/Tribonacci Number/java/src/TribonacciNumber.java
@@ -1,12 +1,51 @@
 public class TribonacciNumber {
 
+    public static int tribonacciEfficient(int n) {
+        if (n < 3) return n == 0 ? 0 : 1;
+
+        int tmp, x = 0, y = 1, z = 1;
+        for(int i = 3; i <= n; i++) {
+            tmp = x + y + z;
+            x = y;
+            y = z;
+            z = tmp;
+        }
+
+        return z;
+    }
+
+    public static int tribonacciMemoization(int n) {
+
+        int[] dp = new int[n+1];
+
+        dp[1] = 1;
+        dp[2] = 1;
+
+        return helper(n, dp);
+    }
+
+    public static int helper(int n, int[] memo) {
+
+        if (n == 0) {
+            return 0;
+        } else if ( memo[n] != 0 ) {
+            return memo[n];
+        }
+
+        if (memo[n] == 0) {
+            memo[n] = helper(n - 1, memo) + helper(n - 2, memo) + helper(n - 3, memo);
+        }
+
+        return memo[n];
+
+    }
+
 
     public static int tribonacciDynamicProgramming(int n) {
 
         if (n == 0) {
             return 0;
         } else if ( 1 <= n && n <= 2 ) {
-            System.out.println(11111);
             return 1;
         }
 
@@ -27,7 +66,8 @@ public class TribonacciNumber {
         int n = 4;
 
         System.out.println(tribonacciDynamicProgramming(n));
-//        System.out.println(containsDuplicateHashSet(nums));
+        System.out.println(tribonacciMemoization(4));
+        System.out.println(tribonacciEfficient(4));
     }
 
 }
