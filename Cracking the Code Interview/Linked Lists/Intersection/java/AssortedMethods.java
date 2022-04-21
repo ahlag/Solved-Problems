989879b7b71d5d49a98f8d7fea5929aafd64593e
Chang Yong Lik <theeahlag@gmail.com>
2022-04-21 23:08:42 +0900

    Added intersection

---
+++ Cracking the Code Interview/Linked Lists/Intersection/java/AssortedMethods.java
@@ -0,0 +1,13 @@
+public class AssortedMethods {
+
+    public static LinkedListNode createLinkedListFromArray(int[] vals) {
+        LinkedListNode head = new LinkedListNode(vals[0], null, null);
+        LinkedListNode current = head;
+        for (int i = 1; i < vals.length; i++) {
+            current = new LinkedListNode(vals[i], null, current);
+        }
+        return head;
+    }
+
+
+}
