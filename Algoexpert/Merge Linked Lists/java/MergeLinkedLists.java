f956dfc41f57153fa97f8a952c00c7177c01c38d
Chang Yong Lik <theeahlag@gmail.com>
2022-04-27 01:19:46 +0900

    Added reverse linked list

--- Algoexpert/Merge Linked Lists/java/MergeLinkedLists.java
+++ Algoexpert/Merge Linked Lists/java/MergeLinkedLists.java
@@ -20,7 +20,31 @@ public class MergeLinkedLists {
     }
 
     public static void main(String[] args) {
+        LinkedList node1 = new LinkedList(1);
+        LinkedList node2 = new LinkedList(2);
+        LinkedList node3 = new LinkedList(3);
+        LinkedList node4 = new LinkedList(4);
+        LinkedList node5 = new LinkedList(5);
+        LinkedList node6 = new LinkedList(6);
 
+        node1.next = node2;
+        node2.next = node3;
+        node3.next = node4;
+        node4.next = node5;
+        node5.next = node6;
+
+        LinkedList node7 = new LinkedList(7);
+        LinkedList node8 = new LinkedList(8);
+
+        node7.next = node8;
+
+        LinkedList res = mergeLinkedLists(node1, node7);
+        LinkedList head = res;
+
+        while (head != null) {
+            System.out.println(head.val);
+            head = head.next;
+        }
     }
 
 }
